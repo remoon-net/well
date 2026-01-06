bf0b0fe6c33ca8272a2e57dad21946fab07bf88b
shynome <shynome@gmail.com>
2026-01-06 22:15:32 +0800

    改成纯英文

--- db/tables.go
+++ db/tables.go
@@ -7,7 +7,7 @@ const (
 )
 
 const (
-	TransportModeNoWS       = "禁用WebSocket"
-	TransportModeNoWebRTC   = "禁用WebRTC"
-	TransportModeWSRedirect = "启用WebSocket跳转"
+	TransportModeNoWS       = "NoWebSocket"
+	TransportModeNoWebRTC   = "NoWebRTC"
+	TransportModeWSRedirect = "EnableWebSocketRedirect"
 )
